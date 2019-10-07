ccfe6ae17afb0396137597662ac649c9c07244c0
Dan Mace <ironcladlou@gmail.com>
2019-10-07 10:53:32 -0400

    Make logging init less broken
    Paritally fix logging init. This is intended to fix things enough to
    let us move forward to refactoring all logging.

--- cmd/openshift-router/main.go
+++ cmd/openshift-router/main.go
@@ -25,6 +25,12 @@ func main() {
 	cmd := CommandFor(filepath.Base(os.Args[0]))
 	cmd.SilenceUsage = true
 	cmd.SilenceErrors = true
+
+	if err := cmd.ParseFlags(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
