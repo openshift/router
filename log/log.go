98a107c142b10167f1475748c8a9cf208fdc37aa
Dan Mace <ironcladlou@gmail.com>
2019-11-11 17:35:27 -0500

    simplify logger instantiation

--- log/log.go
+++ log/log.go
@@ -1,15 +1,9 @@
 package log
 
 import (
-	"github.com/go-logr/logr"
-
 	"k8s.io/klog/klogr"
 )
 
 // Logger is the root logger which should be used by all
 // other packages in the codebase.
-var Logger logr.Logger
-
-func init() {
-	Logger = klogr.New()
-}
+var Logger = klogr.New()
