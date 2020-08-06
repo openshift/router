ab09f77e927092bd7d02762cca78239468e5bba9
Stephen Greene <sgreene@redhat.com>
2020-08-06 10:15:04 -0400

    Add new router failed reload counter metric
    If a router reload fails after a router pod becomes
    ready, we need a way to alert cluster admins that
    newly created route resources are not being applied
    to the cluster.

--- pkg/router/template/router.go
+++ pkg/router/template/router.go
@@ -97,6 +97,8 @@ type templateRouter struct {
 	stateChanged bool
 	// metricReload tracks reloads
 	metricReload prometheus.Summary
+	// metricReloadFails tracks reload failures
+	metricReloadFails prometheus.Counter
 	// metricWriteConfig tracks writing config
 	metricWriteConfig prometheus.Summary
 	// dynamicConfigManager configures route changes dynamically on the
@@ -174,6 +176,12 @@ func newTemplateRouter(cfg templateRouterCfg) (*templateRouter, error) {
 		Help:      "Measures the time spent reloading the router in seconds.",
 	})
 	prometheus.MustRegister(metricsReload)
+	metricReloadFails := prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "template_router",
+		Name:      "reload_fails",
+		Help:      "Tracks the number of failed router reloads",
+	})
+	prometheus.MustRegister(metricReloadFails)
 	metricWriteConfig := prometheus.NewSummary(prometheus.SummaryOpts{
 		Namespace: "template_router",
 		Name:      "write_config_seconds",
@@ -202,6 +210,7 @@ func newTemplateRouter(cfg templateRouterCfg) (*templateRouter, error) {
 		dynamicConfigManager:     cfg.dynamicConfigManager,
 
 		metricReload:      metricsReload,
+		metricReloadFails: metricReloadFails,
 		metricWriteConfig: metricWriteConfig,
 
 		rateLimitedCommitFunction: nil,
@@ -446,6 +455,8 @@ func (r *templateRouter) commitAndReload() error {
 		if r.dynamicConfigManager != nil {
 			r.dynamicConfigManager.Notify(RouterEventReloadError)
 		}
+		// Increment the failed reload counter when a reload fails
+		r.metricReloadFails.Inc()
 		return err
 	}
 
