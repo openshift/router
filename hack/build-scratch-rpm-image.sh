e838390cb7f30b0e6eb95fe47b9a1a50c674b70d
Grant Spence <gspence@redhat.com>
2024-08-16 17:19:41 -0400

    WIP: DO NOT MERGE: Scratch build

---
+++ hack/build-scratch-rpm-image.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" == "" ]]; then
+  echo "ERROR: You must provide a task ID"
+  exit 1
+fi
+taskID=$1
+
+# Make sure we are at the top level dir in the git repo.
+cd $(git rev-parse --show-toplevel)
+
+# Download x86_64 and src RPMs
+mkdir -p $taskID
+cd $taskID
+brew download-task $taskID --skip="debug[source|info]-*" --arch=x86_64 #--arch=src
+cd -
+
+rpmPath=${taskID}/*.x86_64.rpm
+rpmName=$(basename ${rpmPath})
+
+echo "Building scratch RPM image"
+podman build --build-arg=RPM=${rpmName} -t quay.io/openshift/network-edge-testing:router-scratch-rpm-$rpmName -f- ./${taskID} <<EOF
+FROM scratch
+ARG RPM
+COPY \${RPM} /
+EOF
+
+echo "Pushing scratch RPM image"
+podman push quay.io/openshift/network-edge-testing:router-scratch-rpm-$rpmName
+
+# Clean up.
+rm ${taskID} -fr
+
+# Make a file for review with the PR link in it, just for the reviewer's convenience.
+echo "https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=${taskID}" > brewtask.txt
+
+echo "Don't forget to include the git commit patch diff in the distgit HAProxy repo via `git format-patch -1 --subject-prefix=""`"
