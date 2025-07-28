b6b34cb40bada1b5e5ef12e875dc979a5bba6c97
ming1013 <minl@redhat.com>
2025-07-28 20:54:14 +0800

    Add InitTest test function for test cases to get cluster contexts in main.go

--- cmd/example-tests/main.go
+++ cmd/example-tests/main.go
@@ -9,8 +9,10 @@ import (
 	"github.com/openshift-eng/openshift-tests-extension/pkg/cmd"
 	"github.com/openshift-eng/openshift-tests-extension/pkg/extension"
 	g "github.com/openshift-eng/openshift-tests-extension/pkg/ginkgo"
-
 	_ "github.com/openshift/router/test/example"
+
+	exutil "github.com/openshift/router/test/util"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
 func main() {
@@ -32,6 +34,17 @@ func main() {
 	root := &cobra.Command{
 		Long: "OpenShift Tests Extension for Cluster Version Operator",
 	}
+
+	exutil.InitStandardFlags()
+	specs.AddBeforeAll(func() {
+		if err := exutil.InitTest(false); err != nil {
+			panic(err)
+		}
+		e2e.AfterReadingAllFlags(exutil.TestContext)
+		e2e.TestContext.DumpLogsOnFailure = true
+		exutil.TestContext.DumpLogsOnFailure = true
+	})
+
 	root.AddCommand(cmd.DefaultExtensionCommands(registry)...)
 
 	if err := func() error {
